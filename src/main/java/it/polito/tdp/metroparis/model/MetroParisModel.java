395cc2a619e64451bae732b08c67278d8df07417
mariomastrandrea <mariomastrandrea@mbp-di-mario.homenet.telecomitalia.it>
2021-05-03 11:35:25 +0200

    version at 3 may

--- src/main/java/it/polito/tdp/metroparis/model/MetroParisModel.java
+++ src/main/java/it/polito/tdp/metroparis/model/MetroParisModel.java
@@ -1,14 +1,21 @@
 package it.polito.tdp.metroparis.model;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import org.jgrapht.Graph;
 import org.jgrapht.Graphs;
+import org.jgrapht.event.ConnectedComponentTraversalEvent;
+import org.jgrapht.event.EdgeTraversalEvent;
+import org.jgrapht.event.TraversalListener;
+import org.jgrapht.event.VertexTraversalEvent;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.SimpleGraph;
+import org.jgrapht.traverse.BreadthFirstIterator;
 
 import it.polito.tdp.metroparis.db.MetroDAO;
 
@@ -17,6 +24,7 @@ public class MetroParisModel
 	private Graph<Fermata, DefaultEdge> grafo;
 	private MetroDAO metroDao;
 	private Map<Integer, Fermata> fermateById;
+	private Map<Fermata, Fermata> predecessore;
 
 	
 	public MetroParisModel()
@@ -64,7 +72,140 @@ public class MetroParisModel
 			Fermata arrivo = c.getStazA();
 			this.grafo.addEdge(partenza, arrivo);
 		}
+		
+		System.out.println("Grafo creato!");
+		
+		////////////////////////////////
+		
+		/*
+		Fermata f = null;
+		
+		Set<DefaultEdge> archiAdiacenti = this.grafo.edgesOf(f);
+		
+		for(DefaultEdge e : archiAdiacenti)
+		{
+			// (essendo un grafo non orientato, non sappiamo se un arco è stato
+			//	memorizzato in una direzione o nell'altra
+			Fermata f1 = this.grafo.getEdgeSource(e);
+			//oppure
+			Fermata f2 = this.grafo.getEdgeTarget(e);
+					
+			if(f1.equals(f2))
+			{
+				//f2 è quello che mi serve
+			}
+			else 
+			{
+				//f1 è quello che mi serve
+			}
+			
+			//oppure...
+			f1 = Graphs.getOppositeVertex(this.grafo, e, f);		
+		}
+		
+		//oppure, più semplicemente...
+		List<Fermata> fermateAdiacenti = Graphs.successorListOf(this.grafo, f);
+		*/
+		
 	}
+
+	
+	public List<Fermata> fermateRaggiungibiliDa(Fermata partenza)
+	{
+		BreadthFirstIterator<Fermata, DefaultEdge> bfv = new BreadthFirstIterator<>(this.grafo, partenza);
+		//DepthFirstIterator<Fermata, DefaultEdge> dfv = new DepthFirstIterator<>(this.grafo, partenza);
+		
+		this.predecessore = new HashMap<>();
+		//this.predecessore.put(partenza, null);
+		
+		bfv.addTraversalListener(new TraversalListener<Fermata, DefaultEdge>()
+		{	
+			@Override
+			public void vertexTraversed(VertexTraversalEvent<Fermata> e)
+			{
+				/*
+				Fermata nuova = e.getVertex();
+				Fermata precedente = vertice adiacente a 'nuova' che sia già raggiunto
+						
+				predecessore.put(nuova, precedente);
+				*/
+				//meglio analizzare gli archi attraversati 
+			}
+			
+			@Override
+			public void vertexFinished(VertexTraversalEvent<Fermata> e) { }
+			
+			@Override
+			public void edgeTraversed(EdgeTraversalEvent<DefaultEdge> evento) 
+			{
+				/*
+				DefaultEdge arcoAttraversato = evento.getEdge();
+				
+				Fermata a = grafo.getEdgeSource(arcoAttraversato);
+				Fermata b = grafo.getEdgeTarget(arcoAttraversato);
+				
+				// 1) ho scoperto 'a' arrivando da 'b' (se 'b' lo conoscevo già)
+				if(predecessore.containsKey(b) && !predecessore.containsKey(a))
+				{
+					predecessore.put(a,b);
+				}
+				else if(predecessore.containsKey(a) && !predecessore.containsKey(b))	// 2) ho scoperto 'b' arrivando da 'a' (se 'a' lo conoscevo già)
+				{
+					predecessore.put(b,a);
+				}
+				*/
+				//in realtà basta usare il metodo getParent() del BreadthFirstIterator 
+				// (ma che non è presente in DepthFirstIterator)
+			}
+			
+			@Override
+			public void connectedComponentStarted(ConnectedComponentTraversalEvent e) { }
+						
+			@Override
+			public void connectedComponentFinished(ConnectedComponentTraversalEvent e) { }
+		});
+		
+		List<Fermata> result = new ArrayList<>();
+		
+		while(bfv.hasNext())
+		{
+			Fermata f = bfv.next();
+			result.add(f);
+			Fermata parent = bfv.getParent(f);
+			this.predecessore.put(f, parent);
+		}
+		
+		return result;
+	}
+	
+	public Fermata trovaFermata(String nome)
+	{
+		for(Fermata f : this.grafo.vertexSet())
+			if(f.getNome().equals(nome))
+				return f;
+		
+		
+		return null;
+	}
+	
+	public List<Fermata> trovaCammino(Fermata partenza, Fermata arrivo)
+	{
+		fermateRaggiungibiliDa(partenza);
+		
+		LinkedList<Fermata> result = new LinkedList<>();
+		
+		//parto dall'arrivo e vado a ritroso
+		Fermata f = arrivo;
+		
+		while(f != null) {
+			
+			result.addFirst(f);
+			f = predecessore.get(f);
+		}
+		
+		return result;
+	}
+	
 
 	public String getGrafo()
 	{
--- src/main/java/it/polito/tdp/metroparis/model/TestModel.java
+++ src/main/java/it/polito/tdp/metroparis/model/TestModel.java
@@ -1,5 +1,7 @@
 package it.polito.tdp.metroparis.model;
 
+import java.util.List;
+
 public class TestModel
 {
 
@@ -8,7 +10,32 @@ public class TestModel
 		MetroParisModel model = new MetroParisModel();
 		
 		model.creaGrafo();
-		System.out.println(model.getGrafo());
+		
+		Fermata p = model.trovaFermata("La Fourche");
+		if(p == null)
+			System.out.println("Fermata non trovata");
+		else
+		{
+			/*
+			List<Fermata> fermateRaggungibili = model.fermateRaggiungibiliDa(p);
+			
+			System.out.println("Fermata raggiungibili da " + p + ":\n");
+			
+			for(Fermata f : fermateRaggungibili)
+				System.out.println("- "+f);
+			 */
+		}	
+		
+		System.out.println();
+		
+		Fermata a = model.trovaFermata("Temple");
+		
+		List<Fermata> percorso = model.trovaCammino(p, a);
+		
+		int count = 1;
+		for(Fermata f : percorso)
+			System.out.println(count++ +") "+f);
+		
 	}
 
 }
